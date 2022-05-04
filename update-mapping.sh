2d44b80f89335f9786222d7e016a7cac3874782d
dakloifarwa <16627310+dakloifarwa@users.noreply.github.com>
2022-05-04 22:18:56 +0200

    Clean up after modifying

--- update-mapping.sh
+++ update-mapping.sh
@@ -5,4 +5,5 @@ echo 'auth sufficient pam_u2f.so authfile=/etc/u2f_mappings cue' > common-u2f
 for f in gdm-password lightdm sddm sudo login polkit-1 xscreensaver; do
 mv $f $f~
 awk '/@include common-auth/ {print "@include common-u2f"}; {print}' $f~ > $f
+rm $f~
 done
